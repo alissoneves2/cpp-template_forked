8fcb1728d3ad4c7163ed569d2687710c86488717
alissoneves <alissoneves_it@outlook.com>
2026-03-21 12:17:53 -0300

    testando webhook v4

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,5 @@ int main()
     std::cout << "Service 2 started\n";
     std::cout << "Service 3 started\n";
     std::cout << "Service 4 started\n";
-    std::cout << "Service 5 started\n";
     return 0;
 }
